5c49c1242fedf00bfee07229ec1a180b38a434e3
throne <contact@throne.work>
2024-07-26 18:11:45 -0700

    mobile

--- template-parts/overlay-cart-drawer.php
+++ template-parts/overlay-cart-drawer.php
@@ -2,7 +2,5 @@
     <div class="top">
         <h2>Cart</h2> <span class="close-drawer">Close</span>
     </div>
-    <div class="mini-cart-holder">
-        <?php woocommerce_mini_cart(); ?>
-    </div>
+    <?php woocommerce_mini_cart(); ?>
 </div>
